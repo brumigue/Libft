988f1b4e0848cd9c2b6566274515c6b045def91a
Bruno Miguel Luque <brumigue@cbr15s6.42barcelona.com>
2024-06-23 05:18:36 +0200

    L+A-B

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brumigue <brumigue@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 23:32:57 by brumigue          #+#    #+#             */
+/*   Updated: 2024/06/23 04:48:36 by brumigue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(void *dest, const void *src, size_t n)
+{
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
+
+	ptr1 = dest;
+	ptr2 = src;
+	while (n--)
+	{
+		if (*ptr1 != *ptr2)
+			return (*ptr1 - *ptr2);
+		ptr1++;
+		ptr2++;
+	}
+	return (0);
+}
